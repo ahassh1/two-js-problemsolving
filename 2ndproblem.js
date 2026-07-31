11d56e9f2c3df45cc6a06b22233816bd72bb32ae
ahassh1 <ahasanhabibpolash420@gmail.com>
2026-07-31 22:43:40 +0600

    Elevator Weight Safety Checker

---
+++ 2ndproblem.js
@@ -0,0 +1,17 @@
+function isElevatorSafe(weights) {
+    if(!Array.isArray(weights)){
+        return  "Invalid";
+    }
+    let totalWeights = 0;
+     for(const weight of weights){
+        totalWeights += weight;
+     }
+         if(totalWeights <= 400){
+            return true;
+        }
+        else{
+            return false;
+        }
+   
+}
+console.log(isElevatorSafe([120,200]));
