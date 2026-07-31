e01d7377ce71930b4f519e33f7e7e1beda5fd81b
ahassh1 <ahasanhabibpolash420@gmail.com>
2026-07-31 21:43:54 +0600

    Match Winner problem done

---
+++ 1stproblem.js
@@ -0,0 +1,16 @@
+function matchWinner(teamAGoals, teamBGoals) {
+    if(typeof teamAGoals !== "number" || typeof teamBGoals !== "number"){
+        return "Invalid";
+    }
+    if(teamAGoals>teamBGoals){
+        return "Team A Won" ;
+    } 
+    else if(teamBGoals>teamAGoals){
+        return "Team B Won";
+    }
+    else{
+         return "Draw";
+    }
+}
+   console.log(matchWinner("3",6));
+   
